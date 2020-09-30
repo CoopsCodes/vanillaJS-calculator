c647fc4edc503f7423cea10eda6d3ad28367ee7f
Cooper Viktor <im.cooperviktor@gmail.com>
2020-09-30 17:48:48 +1000

    all basic functionality working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const numberButtons = document.querySelectorAll("[data-number]");
 const operandButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
 const allClearButton = document.querySelector("[data-all-clear]");
 const prevOpTextEl = document.querySelector("[data-prev-operand]");
 const currOpTextEl = document.querySelector("[data-curr-operand]");
@@ -18,19 +19,55 @@ class Calculator {
 		this.operation = undefined;
 	}
 
-	delete() {}
+	delete() {
+		this.currOperand = this.currOperand.toString().slice(0, -1);
+	}
 
 	appendNumber(number) {
 		if (number === "." && this.currOperand.includes(".")) return;
 		this.currOperand = this.currOperand.toString() + number.toString();
 	}
 
-	chooseOperation(operation) {}
+	chooseOperation(operation) {
+		if (this.currOperand === "") return;
+		if (this.prevOperand !== "") {
+			this.compute();
+		}
+		this.operation = operation;
+		this.prevOperand = this.currOperand;
+		this.currOperand = "";
+	}
 
-	compute() {}
+	compute() {
+		let computation;
+		const prev = parseFloat(this.prevOperand);
+		const curr = parseFloat(this.currOperand);
+		if (isNaN(prev) || isNaN(curr)) return;
+		switch (this.operation) {
+			case "+":
+				computation = prev + curr;
+				break;
+			case "-":
+				computation = prev - curr;
+				break;
+			case "*":
+				computation = prev * curr;
+				break;
+			case "÷":
+				computation = prev / curr;
+				break;
+
+			default:
+				return;
+		}
+		this.currOperand = computation;
+		this.operation = undefined;
+		this.prevOperand = "";
+	}
 
 	updateDisplay() {
 		this.currOpTextEl.innerText = this.currOperand;
+		this.prevOpTextEl.innerText = this.prevOperand;
 	}
 }
 
@@ -39,13 +76,29 @@ const calculator = new Calculator(prevOpTextEl, currOpTextEl);
 
 numberButtons.forEach((button) => {
 	button.addEventListener("click", () => {
-		console.log(button);
 		calculator.appendNumber(button.innerText);
 		calculator.updateDisplay();
 	});
 });
 
+operandButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		calculator.chooseOperation(button.innerText);
+		calculator.updateDisplay();
+	});
+});
+
+equalsButton.addEventListener("click", (button) => {
+	calculator.compute();
+	calculator.updateDisplay();
+});
+
 allClearButton.addEventListener("click", () => {
 	calculator.clear();
+	calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", () => {
+	calculator.delete();
 	calculator.updateDisplay();
 });
